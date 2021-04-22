494d91b7d130982078c0ad820ea76532e93a020a
AFIKPAROBO EVUE UZUAZOBONA <andre3000real@yahoo.com>
2021-04-22 08:51:28 +0100

    Added initial JavaScript files to the Project Explorer Project

--- app/server/models/projects.js
+++ app/server/models/projects.js
@@ -13,25 +13,68 @@ class Project {
 
 class Projects extends DataModel {
     validate(obj) {
+        this.errors = [];
+        let msg;
+        let isEmpty; 
+        let checkAuthors = Array.isArray(obj.authors); 
+        let checkTags = Array.isArray(obj.tags);
 
-let checkAuthors = Array.isArray(obj.authors);
-let checkTags = Array.isArray(obj.tags);
+        for (const x in obj){
+            if (Object.hasOwnProperty.call(obj, x)){
+                const item = obj[x];
+                if (item == ""){
+                    isEmpty = true;
+                    msg = x + " should not be empty"
+                    this.errors.push(msg)
+                }
+            }
+        }
 
-let value = true;
-for (const item in obj) {
-    if (!obj[item]  || obj[item] === null){
-        value =false;
+        for (let index = 0; index < this.data.length; index++){
+            const item = this.data[index];
+
+            if (obj.authors == item.authors){
+                checkAuthors = true;
+                msg = "Authors should be an array";
+                this.errors.push(msg);
+            }
+        }
+        
+        for (let index = 0; index < this.data.length; index++){
+            const item = this.data[index];
+
+            if (obj.tags == item.tags){
+                checkTags = true;
+                msg = "Tags should be an array";
+                this.errors.push(msg);
+            }
+        }
+
+        if (isEmpty || checkAuthors || checkTags){
+            return true} else {
+                return false;
+        }
     }
 }
 
+//let checkAuthors = Array.isArray(obj.authors);
+//let checkTags = Array.isArray(obj.tags);
 
-  if (value && checkAuthors && checkTags){
-    return true
-  }
+//let value = true;
+//for (const item in obj) {
+    //if (!obj[item]  || obj[item] === null){
+        //value =false;
+    //}
+//}
 
-   return false 
-    }
-}
+
+  //if (value && checkAuthors && checkTags){
+    //return true
+  //}
+
+   //return false 
+
+
 
 
 // Do not worry about the below for now; It is included so that we can test your code
--- app/server/models/users.js
+++ app/server/models/users.js
@@ -42,28 +42,76 @@ let user = this.data.find(item => {
     }
 
     validate(obj) {
-let value = true
-for(const prop in obj){
-    if(obj[prop] === undefined || obj[prop] === null){
-        value = false;
-    }
-}
-let valEmail = this.data.find(item => item.email === obj.email);
-let valMatric = this.data.find(item => item.matricNumber === obj.matricNumber);
-let passCheck = obj.password.length >= 7 ? true : false;
+        this.errors = [];
+        let msg;
+        let isEmpty, userEmail, userMatric, passNot;
 
-   if (passCheck === true && value === true) {
-     if (valEmail === false || valEmail === undefined) {
-         if (valMatric == false || valMatric == undefined) {
-             return true;
+        for (const x in obj){
+            if (Object.hasOwnProperty.call(obj, x)){
+                const item = obj[x];
+                if (item == ""){
+                    isEmpty = true;
+                    msg = x + " should not be empty"
+                    this.errors.push(msg)
+                }
             }
         }
-    }
-    return false; 
 
+        for (let index = 0; index < this.data.length; index++){
+            const item = this.data[index];
+
+            if (obj.email == item.email){
+                userEmail = true;
+                msg = "A user with specified email address already exists";
+                this.errors.push(msg);
+            }
+        }
+
+        for (let index = 0; index < this.data.length; index++){
+            const item = this.data[index];
+
+            if (obj.matricNumber == item.matricNumber){
+                userMatric = true;
+                msg = "A user with specified matric number already exists";
+                this.errors.push(msg);
+            }
+        }
+
+        if (obj.password.length < 7){
+            passNot = true;
+            msg = "Password should have at least 7 characters"
+            this.errors.push(msg);
+        }
+
+        if (isEmpty || userEmail || userMatric || passNot){
+            return false} else {
+                return true;
+        }
     }
 }
 
+//let value = true
+//for(const prop in obj){
+    //if(obj[prop] === undefined || obj[prop] === null){
+       // value = false;
+    //}
+//}
+//let valEmail = this.data.find(item => item.email === obj.email);
+//let valMatric = this.data.find(item => item.matricNumber === obj.matricNumber);
+//let passCheck = obj.password.length >= 7 ? true : false;
+
+   //if (passCheck === true && value === true) {
+     //if (valEmail === false || valEmail === undefined) {
+         //if (valMatric == false || valMatric == undefined) {
+             //return true;
+            //}
+        //}
+    //}
+    //return false;
+
+
+
+    
 // Do not worry about the below for now; It is included so that we can test your code
 // We will cover module exports in later parts of this course
 module.exports = {
